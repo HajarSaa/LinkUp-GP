067804fb53f9ccc75259954970393e3c25bc8982
Omar Mansour <omansour715@gmail.com>
2025-02-07 13:52:38 +0200

    init setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,9 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-    
+
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`server running on port ${port} -> http://127.0.0.1:${port}`);
-  console.log(test);
 });
