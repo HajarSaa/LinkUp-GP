8c397d13e1fffdbba1da28ba91ff12159e09b19b
AhMad AYman <ahmad.ayman62003@gmail.com>
2025-03-02 22:17:23 +0200

    Auth-6

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,21 @@
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import "@fontsource/lato";
-import "./styles/variables.css"
-import "./styles/global.css"
+import "./styles/variables.css";
+import "./styles/global.css";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Register/>
-      <Login />
+        <Routes>
+          {/* تحويل الصفحة الرئيسية لصفحة الـ Login */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* صفحات الـ Authentication */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
     </>
   );
 }
