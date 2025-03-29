50292d43924994244f18ed94ee51b8d3d62dc4d6
ahmad14ayman <ahmad.ayman62003@gmail.com>
2025-03-29 10:04:47 +0200

    Channels List Design 🎨

--- FrontEnd/src/components/Chat/channel/header/Header.jsx
+++ FrontEnd/src/components/Chat/channel/header/Header.jsx
@@ -13,115 +13,120 @@ import HuddleModal from "../../../UI/Modal/channel/HuddleModal/HuddleModal";
 import { openMenu } from "../../../../API/redux/chat/channel/channelMenuSlice";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { openHuddleModal } from "../../../../API/redux/modals/huddleSlice";
+import ChannelDetails from "../../../UI/Modal/channel/ChannelModal/ChannelDetails";
 
 function Header({ channel, user }) {
   const dispatch = useDispatch();
   const membersArray =
     channel.members.length > 6 ? channel.members.slice(0, 5) : channel.members;
   return (
-    <div className={styles.channelHeader}>
-      <div className="justify-content-between w-100 topPart">
-        {channel && (
-          <div
-            className={styles.channelInfo}
-            onClick={() =>
-              dispatch(openChannelDetails({ channel, tab: "about" }))
-            }
-          >
-            <span className="align-items-center">
-              <FaHashtag />
-            </span>
-            <span>{channel.name}</span>
-          </div>
-        )}
-        {user && (
-          <div
-            className={styles.userInfo}
-            onClick={() =>
-              // dispatch(openUserDetails({ user, tab: "about" }))
-              console.log("clicked")
-            }
-          >
-            <h2 className={styles.userName}>
-              <span>{user.img}</span>
-              <span>{user.name}</span>
-            </h2>
-          </div>
-        )}
-        <div className={styles.rightSide}>
+    <>
+      <div className={styles.channelHeader}>
+        <div className="justify-content-between w-100 topPart">
           {channel && (
-            <>
-              <div
-                className={styles.membersContainer}
-                onClick={() =>
-                  dispatch(openChannelDetails({ channel, tab: "members" }))
-                }
-              >
-                <div className={styles.avatars}>
-                  {membersArray.map((member, index) => (
-                    <div
-                      key={index}
-                      className={styles.avatar}
-                      style={{ zIndex: `${100 - index}` }}
-                    >
-                      <img
-                        src={member.avatar}
-                        alt={member.name}
-                        className={styles.avatarImg}
-                      />
-                    </div>
-                  ))}
-                </div>
-                <span className={styles.memberCount}>
-                  {channel.members.length.toLocaleString()}
-                </span>
-              </div>
-              <div className={styles.huddle}>
-                <div className={styles.huddleButton}>
-                  <span className={styles.icon}>
-                    <MdHeadset />
-                  </span>
-                  <span
-                    className="iconsPadding align-items-center"
-                    onClick={() => dispatch(openHuddleModal())}
-                  >
-                    <FiChevronDown className={styles.arrow} />
-                  </span>
-                </div>
-                <HuddleModal />
-              </div>
-              <div className={styles.menu}>
-                <div
-                  className={styles.menuButton}
-                  onClick={() => dispatch(openMenu())}
-                >
-                  <BsThreeDotsVertical />
-                </div>
-                <ChannelOptionModal />
-              </div>
-            </>
+            <div
+              className={styles.channelInfo}
+              onClick={() =>
+                dispatch(openChannelDetails({ channel, tab: "about" }))
+              }
+            >
+              <span className="align-items-center">
+                <FaHashtag />
+              </span>
+              <span>{channel.name}</span>
+            </div>
           )}
-          {user && <ChannelOptionModal user={user} />}
+          {user && (
+            <div
+              className={styles.userInfo}
+              onClick={() =>
+                // dispatch(openUserDetails({ user, tab: "about" }))
+                console.log("clicked")
+              }
+            >
+              <h2 className={styles.userName}>
+                <span>{user.img}</span>
+                <span>{user.name}</span>
+              </h2>
+            </div>
+          )}
+          <div className={styles.rightSide}>
+            {channel && (
+              <>
+                <div
+                  className={styles.membersContainer}
+                  onClick={() =>
+                    dispatch(openChannelDetails({ channel, tab: "members" }))
+                  }
+                >
+                  <div className={styles.avatars}>
+                    {membersArray.map((member, index) => (
+                      <div
+                        key={index}
+                        className={styles.avatar}
+                        style={{ zIndex: `${100 - index}` }}
+                      >
+                        <img
+                          src={member.avatar}
+                          alt={member.name}
+                          className={styles.avatarImg}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <span className={styles.memberCount}>
+                    {channel.members.length.toLocaleString()}
+                  </span>
+                </div>
+                <div className={styles.huddle}>
+                  <div className={styles.huddleButton}>
+                    <span className={styles.icon}>
+                      <MdHeadset />
+                    </span>
+                    <span
+                      className="iconsPadding align-items-center"
+                      onClick={() => dispatch(openHuddleModal())}
+                    >
+                      <FiChevronDown className={styles.arrow} />
+                    </span>
+                  </div>
+                  <HuddleModal />
+                </div>
+                <div className={styles.menu}>
+                  <div
+                    className={styles.menuButton}
+                    onClick={() => dispatch(openMenu())}
+                  >
+                    <BsThreeDotsVertical />
+                  </div>
+                  <ChannelOptionModal />
+                </div>
+              </>
+            )}
+            {user && <ChannelOptionModal user={user} />}
+          </div>
+        </div>
+        <div className={`w-100 align-items-center ${styles.bottomPart}`}>
+          <div className={`${styles.tab} ${styles.activeTab}`}>
+            <span className="align-items-center rotateY-180">
+              <TbMessageCircleFilled />
+            </span>
+            <span>Messages</span>
+          </div>
+          <div className={styles.tab}>
+            <span className="align-items-center">
+              <RiStickyNoteAddLine />
+            </span>
+            <span>Add canvas</span>
+          </div>
+          <span className={styles.plus}>
+            <AiOutlinePlus />
+          </span>
         </div>
       </div>
-      <div className={`w-100 align-items-center ${styles.bottomPart}`}>
-        <div className={`${styles.tab} ${styles.activeTab}`}>
-          <span className="align-items-center rotateY-180">
-            <TbMessageCircleFilled />
-          </span>
-          <span>Messages</span>
-        </div>
-        <div className={styles.tab}>
-          <span className="align-items-center">
-            <RiStickyNoteAddLine />
-          </span>
-          <span>Add canvas</span>
-        </div>
-        <span className={styles.plus}>
-          <AiOutlinePlus />
-        </span>
-      </div>
-    </div>
+      {/* modals */}
+      <ChannelDetails/>
+    </>
   );
 }
 
