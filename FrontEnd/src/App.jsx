d3951e076a69a88bb9efb5dd173df2a497dccf34
Your Name <you@example.com>
2025-04-28 19:27:02 +0300

    Workspace Landing Page 👋�

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,22 +4,26 @@ import "./styles/classes.css";
 import "./styles/global.css";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import MainLayout from "./layouts/MainLayout/MainLayout";
-import BrowseChannels from './pages/dashboard/BrowseChannels/BrowseChannels';
-import DmPage from "./pages/dashboard/DmPage";
+// import MainLayout from "./layouts/MainLayout/MainLayout";
+// import BrowseChannels from "./pages/dashboard/BrowseChannels/BrowseChannels";
+// import DmPage from "./pages/dashboard/DmPage";
+// import ChannelPage from "./pages/dashboard/ChannelPage";
 import { Routes, Route } from "react-router-dom";
-import ChannelPage from "./pages/dashboard/ChannelPage";
+import WorkspaceLanding from "./pages/WorkspaceCreation/WorkspaceLanding/WorkspaceLanding";
+import CreateWorkspace from "./pages/WorkspaceCreation/CreateWorkspace";
 function App() {
   return (
     <div className="app__body">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<MainLayout />}>
+        {/* <Route path="/" element={<MainLayout />}>
           <Route path="dm/:id" element={<DmPage />} />
           <Route path="channels/:id" element={<ChannelPage />} />
           <Route path="browse-channels" element={<BrowseChannels />} />
-        </Route>
+        </Route> */}
+        <Route path="/" element={<WorkspaceLanding />} />
+        <Route path="/create-workspace" element={<CreateWorkspace />} />
       </Routes>
     </div>
   );
