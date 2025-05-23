33a92860d9523c96785af6db92d731fb36f2018a
Your Name <you@example.com>
2025-05-23 23:12:36 +0300

    Edit workspace service

--- FrontEnd/src/API/services/workspaceService.js
+++ FrontEnd/src/API/services/workspaceService.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 
 const API_BASE_URL = "https://link-up-beige.vercel.app/api/v1";
+export const getWorkspace = async (work_id) => {
+  const axios_response = await axios.get(
+    `${API_BASE_URL}/workspaces/${work_id}`,
+    {
+      withCredentials: true,
+    }
+  );
+  const response = axios_response.data;
+  return response.data;
+};
+////////////////////////////////////////////////////////////////////
 export const createWorkspaceService = async (name) => {
   const response = await axios.post(
     `${API_BASE_URL}/workspaces`,
@@ -26,13 +37,3 @@ export const createWorkspaceService = async (name) => {
     }
   }
 ////////////////////////////////////////////////////////////////////
-export const getWorkspace = async (work_id) => {
-  const axios_response = await axios.get(
-    `${API_BASE_URL}/workspaces/${work_id}`,
-    {
-      withCredentials: true,
-    }
-  );
-  const response = axios_response.data;
-  return response.data;
-};
