1d00bc76018acb1cb559ebebc66dd4bc8a1b1976
Omar <omansour715@gmail.com>
2025-06-10 19:33:20 +0300

    ✨ Refactor CORS configuration to use a dynamic origin check for improved security

--- Backend/servers/socketServer.js
+++ Backend/servers/socketServer.js
@@ -8,7 +8,21 @@ const socketServer = (httpServer) => {
     pingTimeout: 10000,
     pingInterval: 20000,
     cors: {
-      origin: ["http://localhost:3000", "https://link-up-beige.vercel.app"],
+      origin: function (origin, callback) {
+        const allowedOrigins = [
+          "http://localhost:5173",
+          "http://localhost:3000",
+          "http://127.0.0.1:5173",
+          // the frontend URL for production(if any)
+        ];
+
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, origin);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       methods: ["GET", "POST"],
       credentials: true,
     },
