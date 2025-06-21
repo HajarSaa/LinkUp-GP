527db9f665e49cc304ea9daac96f29dc0389216b
Alaa <arwaalsoudy@gmail.com>
2025-06-21 09:59:34 +0300

    Finishing (create,delete,join,leave) channel handler events✔

--- FrontEnd/src/API/redux_toolkit/api_data/channelSlice.js
+++ FrontEnd/src/API/redux_toolkit/api_data/channelSlice.js
@@ -9,52 +9,14 @@ const channelSlice = createSlice({
     setChannel: (state, action) => {
       state.channel = action.payload;
     },
+    updateChannelMembers: (state, action) => {
+      if (state.channel) {
+        state.channel.members = action.payload;
+      }
+    },
   },
 });
 
-export const { setChannel, setChanLoading, setChanError } = channelSlice.actions;
+export const { setChannel, setChanLoading, setChanError, updateChannelMembers } = channelSlice.actions;
 
 export default channelSlice.reducer;
-
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   channel: null,
-//   channelList: [],
-// };
-
-// const channelSlice = createSlice({
-//   name: "channel",
-//   initialState,
-//   reducers: {
-//     setChannel: (state, action) => {
-//       state.channel = action.payload;
-//     },
-//     setChannelList: (state, action) => {
-//       state.channelList = action.payload;
-//     },
-//     addChannel: (state, action) => {
-//       state.channelList.push(action.payload);
-//     },
-//     updateChannel: (state, action) => {
-//       const index = state.channelList.findIndex(c => c._id === action.payload._id);
-//       if (index !== -1) {
-//         state.channelList[index] = { ...state.channelList[index], ...action.payload };
-//       }
-//     },
-//     removeChannel: (state, action) => {
-//       state.channelList = state.channelList.filter(c => c._id !== action.payload);
-//     },
-//   },
-// });
-
-// export const {
-//   setChannel,
-//   setChannelList,
-//   addChannel,
-//   updateChannel,
-//   removeChannel,
-// } = channelSlice.actions;
-
-// export default channelSlice.reducer;
