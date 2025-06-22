f0157d9d3f8a03458ce21569a53003f6134d6a95
AhMad AYman Makhlouf <ahmad.ayman62003@gmail.com>
2025-06-22 20:37:38 +0300

    Finish Reactions ✅✅

--- FrontEnd/src/components/Chat/Reactions/Reactions.jsx
+++ FrontEnd/src/components/Chat/Reactions/Reactions.jsx
@@ -2,11 +2,13 @@ import PropTypes from "prop-types";
 import styles from "./Reactions.module.css";
 import { MdOutlineAddReaction } from "react-icons/md";
 import { openEmojiPicker } from "../../../API/redux_toolkit/modals/emojiPickerSlice";
-import { useDispatch } from "react-redux";
-import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useRef } from "react";
 import useGetMessageReactions from "../../../API/hooks/reactions/useGetMessageReactions";
 import { getEmojiPickerPosition } from "../../../utils/modalsUtils";
 import useToggleReaction from "../../../API/hooks/reactions/useToggleReaction";
+import { Tooltip } from "react-tooltip";
+
 
 function Reactions({ messageId }) {
   const dispatch = useDispatch();
@@ -14,6 +16,7 @@ function Reactions({ messageId }) {
   const { mutate: toggleThisReact } = useToggleReaction();
   const { data, isLoading, isError } = useGetMessageReactions(messageId);
   const reactionsObj = data?.groupedReactions || {};
+  const { workspace } = useSelector((state) => state.workspace);
 
   function openEmojies() {
     const rect = add_react_ref.current.getBoundingClientRect();
@@ -39,19 +42,45 @@ function Reactions({ messageId }) {
     <>
       {reactions.length !== 0 && (
         <div className={styles.reactions}>
-          {reactions.map(([emoji, { count }]) => (
-            <div
-              key={emoji}
-              className={styles.react}
-              onClick={() => removeThisReact(emoji)}
-            >
-              {/* <div className={styles.react_emoji}>{emoji}</div> */}
-              <div className={styles.react_emoji}>
-                <img src={emoji} alt="" />
-              </div>
-              <div className={styles.react_count}>{count}</div>
-            </div>
-          ))}
+          {reactions.map(([emoji, { count, UserProfiles }]) => {
+            const memberNames = UserProfiles.map(
+              ({ id }) =>
+                workspace.members.find((member) => member.user === id)?.userName
+            )
+              .filter(Boolean)
+              .join(" , ");
+
+            return (
+              <React.Fragment key={emoji}>
+                <div
+                  data-tooltip-id={`reaction-${emoji}`}
+                  className={styles.react}
+                  onClick={() => removeThisReact(emoji)}
+                >
+                  <div className={styles.react_emoji}>
+                    <img src={emoji} alt="" />
+                  </div>
+                  <div className={styles.react_count}>{count}</div>
+                </div>
+
+                <Tooltip
+                  id={`reaction-${emoji}`}
+                  place="top"
+                  className="custom-tooltip"
+                  content={
+                    <div className={styles.toolTip}>
+                      <div className={styles.react_image}>
+                        <img src={emoji} alt=" " />
+                      </div>
+                      <div className={styles.names}>
+                        {memberNames || "No one yet"}
+                      </div>
+                    </div>
+                  }
+                />
+              </React.Fragment>
+            );
+          })}
 
           <div
             className={styles.add_react}
