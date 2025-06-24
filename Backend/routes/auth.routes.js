a2979aa4fcc1aa287decba48d98e5b30bb0fdb30
Omar <omansour715@gmail.com>
2025-06-24 18:37:46 +0300

    ✨feat: Add logout functionality

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import expres from "express";
-import { login, signup } from "../controllers/auth.controller.js";
-
+import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protect } from "../middlewares/authMiddleware.js";
 const router = expres.Router();
 
 router.post("/signup", signup).post("/login", login);
+
+router.use(protect);
+router.post("/logout", logout);
 export default router;
