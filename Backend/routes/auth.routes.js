b027f35a709cd55e01bae9616a72c66e1c986144
Omar <omansour715@gmail.com>
2025-06-25 00:57:20 +0300

    ✨feat: Implement password reset functionality with email notifications

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,9 +1,21 @@
 import expres from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  forgotPassword,
+  login,
+  logout,
+  resetPassword,
+  signup,
+  verifyPasswordRestCode,
+} from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/authMiddleware.js";
 const router = expres.Router();
 
-router.post("/signup", signup).post("/login", login);
+router
+  .post("/signup", signup)
+  .post("/login", login)
+  .post("/forgotPassword", forgotPassword)
+  .post("/verifyResetCode", verifyPasswordRestCode)
+  .post("/resetPassword", resetPassword);
 
 router.use(protect);
 router.post("/logout", logout);
