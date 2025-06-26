7a3bf993a04dea18e77914abcac302fdf72b319d
Asmaa-Elkilany <asmaa123amk@gmail.com>
2025-06-26 18:45:59 +0300

    Adding save for later endpoint

--- Backend/routes/laterItem.routes.js
+++ Backend/routes/laterItem.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getLaterItems } from "../controllers/laterItem.controller.js";
+import {
+  getLaterItems,
+  toggleSaveForLater,
+  updateLaterItemStatus,
+  setLaterReminder,
+  removeLaterReminder,
+} from "../controllers/laterItem.controller.js";
 import {
   protect,
   protectAttchWorkspace,
@@ -11,5 +17,10 @@ router.use(protect);
 router.use(protectAttchWorkspace);
 
 router.get("/", getLaterItems);
+router
+  .patch("/:id/toggle", toggleSaveForLater)
+  .patch("/:id/status", updateLaterItemStatus)
+  .patch("/:id/setReminder", setLaterReminder)
+  .patch("/:id/removeReminder", removeLaterReminder);
 
 export default router;
