43fdffc02d57cc2d768c4adbc11e2b56d3c84f00
AhMad AYman Makhlouf <ahmad.ayman62003@gmail.com>
2025-06-29 02:15:09 +0300

    Logout btn ✅

--- FrontEnd/src/pages/Landing/Landing.jsx
+++ FrontEnd/src/pages/Landing/Landing.jsx
@@ -1,14 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import LandingHeader from "../../components/UI/Landing/LandingHeader";
 import WorkspacesList from "../../components/UI/Landing/WorkspacesList";
 import styles from "./Landing.module.css";
 
 const Landing = function () {
+  const navigateTo = useNavigate();
+
+  function handleLogout() {
+    navigateTo('/login')
+  }
   return (
     <div className={styles.landing_page}>
       <div className={styles.landing_page_container}>
         <LandingHeader />
         <WorkspacesList />
       </div>
+      <button className={styles.logout} onClick={handleLogout}>Logout</button>
     </div>
   );
 };
